2f8bfcb4e9df23191d584abcdf74a0f69477844b
Isaac Marovitz <isaacryu@icloud.com>
2023-11-03 08:44:41 -0400

    MVP

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,9 @@ use iso8601::date;
 use semver::Version;
 use serenity::model::prelude::*;
 use serenity::prelude::*;
-use serenity::Client;
+use shuttle_runtime;
+use shuttle_secrets::SecretStore;
 use std::default::Default;
-use std::env;
 use std::str;
 use std::str::FromStr;
 
@@ -12,7 +12,10 @@ pub mod header;
 
 struct Handler;
 
-const ALLOWED_CHANNELS: [ChannelId; 2] = [ChannelId(1115957389062066218), ChannelId(1115961098961702992)];
+const ALLOWED_CHANNELS: [ChannelId; 2] = [
+    ChannelId(1115957389062066218),
+    ChannelId(1115961098961702992),
+];
 
 #[serenity::async_trait]
 impl EventHandler for Handler {
@@ -22,35 +25,51 @@ impl EventHandler for Handler {
             let filename = attachment.filename.clone();
 
             if content_type == "text/plain; charset=utf-8" && filename.ends_with(".log") {
-                let header = parse_log(attachment.download().await.expect("Failed to download log")).await;
+                let header =
+                    parse_log(attachment.download().await.expect("Failed to download log")).await;
                 let mut id = msg.channel_id;
 
-                msg.
                 match msg.thread.clone() {
-                    None => {println!("Found nothing")}
-                    Some(channel) => {id = channel.parent_id.expect("Failed to get ID")}
+                    None => {
+                        println!("Found nothing")
+                    }
+                    Some(channel) => id = channel.parent_id.expect("Failed to get ID"),
                 }
 
-                if ALLOWED_CHANNELS.contains(id.as_ref()) {
-                    msg.channel_id.send_message(&context, |m| {
-                        m.embed(|e| e
-                            .color(0xC86432)
-                            .field("Whisky Version", header.whisky_version.0, true)
-                            .field("Date", header.date.to_owned(), true)
-                            .field("macOS Version", header.macos_version.0, true)
-                            .field("Wine Version", header.wine_version.0, true)
-                            .field("Windows Version", header.windows_version, true)
-                            .field("Enhanced Sync", header.enhanced_sync, true)
-                            .field("Bottle Name", header.bottle_name.clone(), false)
-                            .field("Bottle URL", format!("`{}`", header.bottle_url), false)
-                            .field("Arguments", format!("`{}`", header.arguments), false)
-                            .footer(|f| {
-                                f.text(format!("Log uploaded by @{}", msg.author.name));
-                                f
-                            }))
-                    }).await.expect("Whee");
+                // if ALLOWED_CHANNELS.contains(id.as_ref()) {
+                if true {
+                    msg.channel_id
+                        .send_message(&context, |m| {
+                            m.embed(|e| {
+                                e.color(0xC86432)
+                                    .field("Whisky Version", header.whisky_version.0, true)
+                                    .field("Date", header.date.to_owned(), true)
+                                    .field("macOS Version", header.macos_version.0, true)
+                                    .field("Wine Version", header.wine_version.0, true)
+                                    .field("Windows Version", header.windows_version, true)
+                                    .field("Enhanced Sync", header.enhanced_sync, true)
+                                    .field("Bottle Name", header.bottle_name.clone(), false)
+                                    .field("Bottle URL", format!("`{}`", header.bottle_url), false)
+                                    .field("Arguments", format!("`{}`", header.arguments), false)
+                                    .footer(|f| {
+                                        f.text(format!("Log uploaded by @{}", msg.author.name));
+                                        f
+                                    })
+                            })
+                        })
+                        .await
+                        .expect("Whee");
                 } else {
-                    msg.channel_id.say(&context, format!("Sorry <@{}>! I can only parse logs in <#{}> and <#{}>.", msg.author.id.0, ALLOWED_CHANNELS[0].0, ALLOWED_CHANNELS[1].0)).await.expect("Failed to send message");
+                    msg.channel_id
+                        .say(
+                            &context,
+                            format!(
+                                "Sorry <@{}>! I can only parse logs in <#{}> and <#{}>.",
+                                msg.author.id.0, ALLOWED_CHANNELS[0].0, ALLOWED_CHANNELS[1].0
+                            ),
+                        )
+                        .await
+                        .expect("Failed to send message");
                 }
             }
         }
@@ -65,20 +84,22 @@ impl EventHandler for Handler {
     }
 }
 
-#[tokio::main]
-async fn main() {
-    let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
+#[shuttle_runtime::main]
+async fn serenity(
+    #[shuttle_secrets::Secrets] secret_store: SecretStore,
+) -> shuttle_serenity::ShuttleSerenity {
+    let token = secret_store.get("DISCORD_TOKEN").unwrap();
 
     let intents = GatewayIntents::GUILD_MESSAGES
         | GatewayIntents::DIRECT_MESSAGES
         | GatewayIntents::MESSAGE_CONTENT;
 
-    let mut client = Client::builder(token, intents)
+    let client = Client::builder(token, intents)
         .event_handler(Handler)
         .await
         .expect("Failed to create client");
 
-    client.start().await.expect("Failed to start client");
+    Ok(client.into())
 }
 
 // TODO: Don't use a Box for this

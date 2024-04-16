b50d7064ff1d3b1b2497cfb07e1aca06d84b9dc7
logan <hahayupbiz@gmail.com>
2024-04-16 19:05:03 -0400

    Fix indentation errors for reply message

--- src/commands/does_my_game_work.rs
+++ src/commands/does_my_game_work.rs
@@ -16,16 +16,16 @@ async fn does_my_game_work(ctx: &Context, msg: &Message) -> CommandResult {
 
     let reply = String::from("Does your game work? Check https://docs.getwhisky.app/game-support/ first, and search to see if anyone else has this same issue in this Discord or the GitHub repository.
 
-    Can't find your game from those sources? Check here:
-    https://www.protondb.com/
-    https://appdb.winehq.org/
-    https://www.codeweavers.com/compatibility
-    https://www.applegamingwiki.com/wiki/Home
-    https://www.pcgamingwiki.com/wiki/Home
-    
-    If you've checked all of these places and still can't find an answer, feel free to create a support post in #support !
+Can't find your game from those sources? Check here:
+https://www.protondb.com/
+https://appdb.winehq.org/
+https://www.codeweavers.com/compatibility
+https://www.applegamingwiki.com/wiki/Home
+https://www.pcgamingwiki.com/wiki/Home
 
-    As a reminder: Most games with EasyAntiCheat and most other anti-cheats will **NOT** work without workarounds.");
+If you've checked all of these places and still can't find an answer, feel free to create a support post in #support !
+
+As a reminder: Most games with EasyAntiCheat and most other anti-cheats will **NOT** work without workarounds.");
 
     let mut reply_msg = msg.reply(&ctx, reply).await?;
 

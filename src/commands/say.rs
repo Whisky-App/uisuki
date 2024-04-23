520530ab95b9b223c8583fc937ccdcefb7c5876b
Isaac Marovitz <isaacryu@icloud.com>
2024-04-22 23:54:40 -0400

    Whoops

--- src/commands/say.rs
+++ src/commands/say.rs
@@ -3,7 +3,6 @@ use crate::{Context, Error};
 #[poise::command(prefix_command, slash_command, owners_only, guild_only)]
 pub async fn say(ctx: Context<'_>,
     #[description = "Message"] message: String) -> Result<(), Error> {
-    ctx.reply()
     ctx.say(message).await?;
 
     Ok(())

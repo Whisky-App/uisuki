34d305f7b2ae87d0e8e5f24081e7bb4091f9cde2
Isaac Marovitz <isaacryu@icloud.com>
2024-04-23 09:08:09 -0400

    Try again

--- src/commands/game_support.rs
+++ src/commands/game_support.rs
@@ -1,4 +1,3 @@
-use log::info;
 use reqwest::StatusCode;
 use crate::{Context, Error};
 
@@ -6,39 +5,43 @@ use crate::{Context, Error};
 pub async fn game_support(ctx: Context<'_>,
     #[description = "Game Name"] game_name: Option<String>) -> Result<(), Error> {
     // String containing default response
-    let mut message = String::from("https://docs.getwhisky.app/game-support/");
+    let mut message = String::from("https://whisky-app.github.io/whisky-book/game-support/");
 
     match game_name {
         Some(name) => {
-            message.push_str(&name);
+            let esc_name = name.trim().replace(" ", "-").to_lowercase();
+            message.push_str(&esc_name);
 
-            // TODO: Shuttle blocks us from making requests
-            // let resp = reqwest::get(site.clone()).await?;
-            //
-            // info!("{} returned code {}", site, resp.status());
-            //
-            // if resp.status() == StatusCode::OK {
-            //
-            // } else {
-            //     ctx.reply("Hmm, seems that game isn't in our docs.").await?;
-            // }
+            let resp = reqwest::get(message.clone()).await?;
+
+            match resp.status() {
+                StatusCode::OK => {
+                    ctx.reply(message).await?;
+                },
+                StatusCode::NOT_FOUND => {
+                    ctx.reply("Hmm, seems that game isn't in our docs.").await?;
+                },
+                code => {
+                    ctx.reply(format!("Hmm, seems I'm having trouble connecting to docs. ({code})"), ).await?;
+                }
+            }
         },
         None => {}
     }
 
-    if let Context::Prefix(prefix) = ctx {
-        match prefix.msg.clone().referenced_message {
-            Some(parent) => {
-                parent.reply(&ctx, message).await?;
-                prefix.msg.delete(ctx).await?;
-            },
-            None => {
-                ctx.reply(message).await?;
-            }
-        }
-    } else {
-        ctx.reply(message).await?;
-    }
+    // if let Context::Prefix(prefix) = ctx {
+    //     match prefix.msg.clone().referenced_message {
+    //         Some(parent) => {
+    //             parent.reply(&ctx, message).await?;
+    //             prefix.msg.delete(ctx).await?;
+    //         },
+    //         None => {
+    //             ctx.reply(message).await?;
+    //         }
+    //     }
+    // } else {
+    //     ctx.reply(message).await?;
+    // }
 
     Ok(())
 }

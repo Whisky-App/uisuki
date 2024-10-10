d8109ef70774ed02e59668c480dc5c046fe1cf2e
logan <161992562+hahayupgit@users.noreply.github.com>
2024-10-10 16:42:28 -0400

    Add whats_where command, heroic command, add context for replied messages (#19)
    * Create ~heroic command
    
    * Add default reply for ~gs
    
    * Create whats_where command and add reply ctx
    
    Add context for replied command for who invoked the message.
    
    Adds new whats_where command.
    
    * Fix reply command to properly ping replied-to user
    
    * replace GPTK with WhiskyWine
    
    * fix mistakes in rebase

--- src/commands/github.rs
+++ src/commands/github.rs
@@ -2,12 +2,15 @@ use crate::{Context, Error};
 
 #[poise::command(prefix_command, slash_command)]
 pub async fn github(ctx: Context<'_>) -> Result<(), Error> {
-    let message = "https://github.com/Whisky-App/Whisky";
+    let mut message = "https://github.com/Whisky-App/Whisky".to_owned();
 
     if let Context::Prefix(prefix) = ctx {
         match prefix.msg.clone().referenced_message {
             Some(parent) => {
-                parent.reply(&ctx, message).await?;
+                message += "\n\nThis command was invoked by ";
+                message += ctx.author().to_string().as_str();
+                
+                parent.reply_ping(&ctx, message).await?;
                 prefix.msg.delete(ctx).await?;
             },
             None => {
